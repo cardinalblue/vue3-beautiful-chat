0790150a91b66abf5a52a68dba368ec55251cb78
Jaime Cham <jaimejaime@gmail.com>
2022-10-28 19:41:10 +0300

    Styling for button

--- src/icons/ImageIcon.vue
+++ src/icons/ImageIcon.vue
@@ -1,16 +1,14 @@
 <template>
-  <div style="position: relative">
-    <button class="sc-user-input--file-icon-wrapper" type="button">
-      x <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa=""><path fill="currentColor" d="M160 160v704h704V160H160zm-32-64h768a32 32 0 0 1 32 32v768a32 32 0 0 1-32 32H128a32 32 0 0 1-32-32V128a32 32 0 0 1 32-32z"></path><path fill="currentColor" d="M384 288q64 0 64 64t-64 64q-64 0-64-64t64-64zM185.408 876.992l-50.816-38.912L350.72 556.032a96 96 0 0 1 134.592-17.856l1.856 1.472 122.88 99.136a32 32 0 0 0 44.992-4.864l216-269.888 49.92 39.936-215.808 269.824-.256.32a96 96 0 0 1-135.04 14.464l-122.88-99.072-.64-.512a32 32 0 0 0-44.8 5.952L185.408 876.992z"></path></svg>
-      <input
-        id="file-input"
-        accept="image/*"
-        type="file"
-        @change="_handleChange"
-        @click="_handleClick"
-      />
-    </button>
-  </div>
+  <button class="sc-user-input--file-icon-wrapper" type="button">
+    <svg class="svg" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa=""><path fill="currentColor" d="M160 160v704h704V160H160zm-32-64h768a32 32 0 0 1 32 32v768a32 32 0 0 1-32 32H128a32 32 0 0 1-32-32V128a32 32 0 0 1 32-32z"></path><path fill="currentColor" d="M384 288q64 0 64 64t-64 64q-64 0-64-64t64-64zM185.408 876.992l-50.816-38.912L350.72 556.032a96 96 0 0 1 134.592-17.856l1.856 1.472 122.88 99.136a32 32 0 0 0 44.992-4.864l216-269.888 49.92 39.936-215.808 269.824-.256.32a96 96 0 0 1-135.04 14.464l-122.88-99.072-.64-.512a32 32 0 0 0-44.8 5.952L185.408 876.992z"></path></svg>
+    <input
+      id="file-input"
+      accept="image/*"
+      type="file"
+      @change="_handleChange"
+      @click="_handleClick"
+    />
+  </button>
 </template>
 
 <script>
@@ -47,6 +45,8 @@ export default {
   margin: 0px;
   outline: none;
   cursor: pointer;
+
+  width: 22px;
 }
 
 .sc-user-input--file-icon {
@@ -58,5 +58,9 @@ export default {
 
 .sc-user-input--file-icon:hover path {
   filter: contrast(15%);
+}
+
+.svg {
+  display: block; /* So no space underneath */
 }
 </style>
